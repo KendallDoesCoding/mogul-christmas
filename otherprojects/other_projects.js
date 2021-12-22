dc80d2af881b020f30f6e23a04fc617b62f57b5a
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-22 22:34:19 +0530

    experiementing

--- otherprojects/other_projects.js
+++ otherprojects/other_projects.js
@@ -1,1 +1,12 @@
-
+const songs = {
+  "Christmas Song": "0",
+  "Baby It's Cold Outside": "212",
+  "The Most Wonderful Time Of The Year": "374",
+  "Last Christmas": "525",
+  "Little Saint Nick": "808",
+  "All I Want For Christmas": "940"
+  }
+            
+Object.keys(songs).map(song_title => {
+  const seconds = songs[song_title]
+  const outerElem = document.createElement("p")
