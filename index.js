5b48e20a7c06e2b0b31158ebeda1f6c47f1c328f
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 18:08:17 +0530

    adjust start path, (read descripton)
    wasnt concentrating lol

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const songs = {
   "Last Christmas": {start: "525", end: "807"},
   "Little Saint Nick": {start: "808", end: "939"},
   "All I Want For Christmas": {start: "940", end: "1179"},
-  "All of the above": {start: "940", end:"1179"}
+  "All of the above": {start: "0", end:"1179"}
   }
 const songsDOM = document.querySelector(".songs")
 const embed = document.getElementById("embed")
