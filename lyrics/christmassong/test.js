60def4005c98c42fb10c77770dd597391f578efe
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 19:03:22 +0530

    Create test.js

---
+++ lyrics/christmassong/test.js
@@ -0,0 +1,6 @@
+ for (i = 0; i < numArray.length; i++) { 
+
+      var line = document.createElement("p");
+      line.className = "line";
+      line.innerHTML = "|"; 
+      document.body.appendChild(line);
