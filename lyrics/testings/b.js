b43ef18376dd309521d08dee4f23a4f25c9054f7
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 20:17:52 +0530

    test

--- lyrics/testings/b.js
+++ lyrics/testings/b.js
@@ -2,8 +2,27 @@ document.write('\
 \
     <h1>Add your HTML code here</h1>\
 \
-     <p>Notice however, that you have to escape LF's with a '\', just like\
-        demonstrated in this code listing.\
-    </p>\
+<div class="lyrics">
+  <p>Chestnuts roasting on an open fire</p>\
+  <p>Jack Frost nipping at your nose</p>\
+  <p>Yuletide carols being sung by a choir</p>\
+  <p>And folks dressed up like Eskimos</p>\
+  <p>Everybody knows a turkey and some mistletoe</p>\
+  <p>Help to make the season bright</p>\
+  <p>Tiny tots with their eyes all aglow</p>\
+  <p>Will find it hard to sleep tonight</p>\
+  <p>They know that Santa's on his way</p>\
+  <p>He's loaded lots of toys and goodies on his sleigh</p>\
+  <p>And every mother's child is gonna spy</p>\
+  <p>To see if reindeers really know how to fly</p>\
+  <p>And so I'm offering this simple phrase</p>\
+  <p>To kids from one to ninety-two</p>\
+  <p>Although it's been said many times, many ways</p>\
+  <p>Merry Christmas to you</p>\
+  <p>And so I'm offering this simple phrase</p>\
+  <p>To kids from one to ninety-two</p>\
+  <p>Although it's been said many times, many ways</p>\
+  <p>Merry Christmas to you</p>\
+</div>
 \
 ');
