5a698ee651fb260640aca8e64ea05ec8ae1e13ea
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 20:40:15 +0530

    completed

---
+++ lyrics/littlesaintnick/index.js
@@ -0,0 +1,40 @@
+document.write(`
+<div class="lyrics">
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>Christmas comes this time each year</p>
+  <p>Ooh, ooh</p>
+  <p>Well way up North where the air gets cold</p>
+  <p>There's a tale about Christmas</p>
+  <p>That you've all been told</p>
+  <p>And a real famous cat all dressed up in red</p>
+  <p>And he spends the whole year workin' out on his sled</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>Just a little bobsled we call the old Saint Nick</p>
+  <p>But she'll walk a toboggan with a four speed stick</p>
+  <p>She's candy apple red with a ski for a wheel</p>
+  <p>And when Santa hits the gas, man, just watch her peel</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>Run run reindeer</p>
+  <p>Run run reindeer (whoa)</p>
+  <p>Run run reindeer</p>
+  <p>Run run reindeer (he don't miss no one)</p>
+  <p>And haulin' through the snow at a frightening speed</p>
+  <p>With a half a dozen deer with Rudy to lead</p>
+  <p>He's got to wear his goggles 'cause the snow really flies</p>
+  <p>And he's cruisin' every path with a little surprise</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>(Christmas comes this time each year)</p>
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>(Christmas comes this time each year)</p>
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>(Christmas comes this time each year)</p>
+</div>
+`);
