092e26204abc5626b04134755becb663fbf0656b
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 20:20:07 +0530

    Update b.js

--- lyrics/testings/b.js
+++ lyrics/testings/b.js
@@ -2,7 +2,8 @@ document.write('\
 \
     <h1>Add your HTML code here</h1>\
 \
-  <p>Chestnuts roasting on an open fire</p>\
+     <p>Notice however, that you have to escape LF's with a '\', just like\
+        demonstrated in this code listing.\
+    </p>\
 \
-              
 ');
