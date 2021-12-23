25e56c7917e312335c41e8fe94a31d9847555abc
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 20:16:56 +0530

    Update and rename b.html to b.js

--- lyrics/testings/b.html
+++
@@ -1,1 +0,0 @@
-<p>This is my include file</p>
---
+++ lyrics/testings/b.js
@@ -0,0 +1,9 @@
+document.write('\
+\
+    <h1>Add your HTML code here</h1>\
+\
+     <p>Notice however, that you have to escape LF's with a '\', just like\
+        demonstrated in this code listing.\
+    </p>\
+\
+');
