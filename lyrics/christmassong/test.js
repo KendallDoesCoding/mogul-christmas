08d3d54058e09424326218fc68b17a3c03dacf00
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 19:04:28 +0530

    trying to fix issue

--- lyrics/christmassong/test.js
+++ lyrics/christmassong/test.js
@@ -1,6 +1,6 @@
- for (i = 0; i < numArray.length; i++) { 
-
-      var line = document.createElement("p");
-      line.className = "line";
-      line.innerHTML = "|"; 
-      document.body.appendChild(line);
+for ( var i=0, n=numArray.length; i < n; i++) { 
+  var line = document.createElement("p");
+  line.className = "line";
+  line.innerHTML = '|';
+  document.body.appendChild(line);
+}
