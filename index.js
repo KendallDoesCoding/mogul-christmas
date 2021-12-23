056db61615de429399d813987a7d80c03dd96eb7
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 18:05:30 +0530

    fix #26

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const songs = {
   "The Most Wonderful Time Of The Year": {start: "374", end:"524"},
   "Last Christmas": {start: "525", end: "807"},
   "Little Saint Nick": {start: "808", end: "939"},
-  "All I Want For Christmas": {start: "940", end: "1179"}
+  "All I Want For Christmas": {start: "940", end: "1179"},
+  "All of the above": {start: "940", end:"1179"}
   }
 const songsDOM = document.querySelector(".songs")
 const embed = document.getElementById("embed")
