aa76765258d56a95829e844d699a2257447ad480
Kendall Pinto <github@kendalldoescoding.gq>
2021-12-23 21:40:44 +0530

    const all lyrics in one js file

--- lyrics/lyrics.js
+++ lyrics/lyrics.js
@@ -1,1 +1,283 @@
+// The Christmas Song
+document.write(`
+<div class="lyrics">
+  <p>Chestnuts roasting on an open fire</p>
+  <p>Jack Frost nipping at your nose</p>
+  <p>Yuletide carols being sung by a choir</p>
+  <p>And folks dressed up like Eskimos</p>
+  <p>Everybody knows a turkey and some mistletoe</p>
+  <p>Help to make the season bright</p>
+  <p>Tiny tots with their eyes all aglow</p>
+  <p>Will find it hard to sleep tonight</p>
+  <p>They know that Santa's on his way</p>
+  <p>He's loaded lots of toys and goodies on his sleigh</p>
+  <p>And every mother's child is gonna spy</p>
+  <p>To see if reindeers really know how to fly</p>
+  <p>And so I'm offering this simple phrase</p>
+  <p>To kids from one to ninety-two</p>
+  <p>Although it's been said many times, many ways</p>
+  <p>Merry Christmas to you</p>
+  <p>And so I'm offering this simple phrase</p>
+  <p>To kids from one to ninety-two</p>
+  <p>Although it's been said many times, many ways</p>
+  <p>Merry Christmas to you</p>
+</div>
+`);
 
+// Baby It's Cold Outside
+document.write(`
+<div class="lyrics-1">
+    <p>I really can't stay</p>
+    <p>Baby it's cold outside</p>
+    <p>I gotta go away</p>
+    <p>Baby it's cold outside</p>
+    <p>This evening has been</p>
+    <p>Been hoping that you'd dropped in</p>
+    <p>So very nice</p>
+    <p>I'll hold your hands they're just like ice</p>
+    <p>My mother will start to worry</p>
+    <p>Beautiful what's your hurry?</p>
+    <p>My father will be pacing the floor</p>
+    <p>Listen to the fireplace roar</p>
+    <p>So really I'd better scurry</p>
+    <p>Beautiful please don't hurry</p>
+    <p>Well maybe just a half a drink more</p>
+    <p>I'll put some records on while I pour</p>
+    <p>The neighbors might think</p>
+    <p>Baby it's bad out there</p>
+    <p>Say what's in this drink?</p>
+    <p>No cabs to be had out there</p>
+    <p>I wish I knew how</p>
+    <p>Your eyes are like starlight now</p>
+    <p>To break this spell</p>
+    <p>I'll take your hat, your hair looks swell</p>
+    <p>Why thank you</p>
+    <p>I ought to say no, no, no sir</p>
+    <p>Mind if move in closer?</p>
+    <p>At least I'm gonna say that I tried</p>
+    <p>What's the sense of hurting my pride?</p>
+    <p>I really can't stay</p>
+    <p>Baby don't hold out</p>
+    <p>Baby it's cold outside</p>
+    <p>Ah, you're very pushy you know?</p>
+    <p>I like to think of it as opportunistic</p>
+    <p>I simply must go</p>
+    <p>Baby it's cold outside</p>
+    <p>The answer is no</p>
+    <p>But baby it's cold outside</p>
+    <p>The welcome has been</p>
+    <p>How lucky that you dropped in</p>
+    <p>So nice and warm</p>
+    <p>Look out the window at that storm</p>
+    <p>My sister will be suspicious</p>
+    <p>Gosh your lips look delicious!</p>
+    <p>My brother will be there at the door</p>
+    <p>Waves upon a tropical shore</p>
+    <p>My maiden aunt's mind is vicious</p>
+    <p>Gosh your lips are delicious!</p>
+    <p>Well maybe just a cigarette more</p>
+    <p>Never such a blizzard before</p>
+    <p>And I don't even smoke</p>
+    <p>I've got to get home</p>
+    <p>Baby you'll freeze out there</p>
+    <p>Say lend me your comb</p>
+    <p>It's up to your knees out there!</p>
+    <p>You've really been grand,</p>
+    <p>I feel when I touch your hand</p>
+    <p>But don't you see?</p>
+    <p>How can you do this thing to me?</p>
+    <p>There's bound to be talk tomorrow</p>
+    <p>Think of my life long sorrow!</p>
+    <p>At least there will be plenty implied</p>
+    <p>If you caught pneumonia and died!</p>
+    <p>I really can't stay</p>
+    <p>Get over that old out</p>
+    <p>Baby it's cold</p>
+    <p>Baby it's cold outside</p>
+    <p>Okay fine, just another drink then</p>
+    <p>That took a lot of convincing!</p>
+</div>
+`);
+
+// The Most Wonderful Time Of The Year
+document.write(`
+<div class="lyrics-2">
+<p>Its the most wonderful time of the year</p>
+  <p>With the kids jingle belling and everyone telling youll be of good cheer</p>
+  <p>Its the most wonderful time of the year</p>
+  <p>Its the hap- happest season of all</p>
+  <p>With those holiday greetings</p>
+  <p>And gay happy meetings when friends come to call</p>
+  <p>Its the hap- happest season of all</p>
+  <p>There be parties for hosting</p>
+  <p>Marshmellows for tosting</p>
+  <p>And caroling out in the snow</p>
+  <p>There be scary ghost stories and tales of the glories of Christmases</p>
+  <p>Long long ago</p>
+  <p>Its the most wonderful time of the year</p>
+  <p>There be much misoltoeng</p>
+  <p>And hearts will be glowing</p>
+  <p>When loved ones are near</p>
+  <p>Its the most wonderful time of the year</p>
+  <p>There be parties for hosting</p>
+  <p>Marshmellows for tosting</p>
+  <p>And caroling out in the snow</p>
+  <p>There be scary ghost stories</p>
+  <p>And tales of the glories of Christmases long long ago</p>
+  <p>Its the most wonderful time of the year</p>
+  <p>There be much misoletong</p>
+  <p>And hearts will be glowing</p>
+  <p>When loved ones are near</p>
+  <p>Its the most wonderful time</p>
+  <p>Yes the most wonderful time</p>
+  <p>Its the most wonderful time of he year</p>
+</div>
+`);
+
+// Last Christmas
+document.write(`
+<div class="lyrics-3">
+  <p>Last Christmas, I gave you my heart</p>
+  <p>But the very next day, you gave it away</p>
+  <p>This year, to save me from tears</p>
+  <p>I'll give it to someone special</p>
+  <p>Last Christmas, I gave you my heart</p>
+  <p>But the very next day, you gave it away (you gave it away)</p>
+  <p>This year, to save me from tears</p>
+  <p>I'll give it to someone special (special)</p>
+  <p>Once bitten and twice shy</p>
+  <p>I keep my distance, but you still catch my eye</p>
+  <p>Tell me, baby, do you recognize me?</p>
+  <p>Well, it's been a year, it doesn't surprise me</p>
+  <p>"Merry Christmas, " I wrapped it up and sent it</p>
+  <p>With a note saying "I love you, " I meant it</p>
+  <p>Now I know what a fool I've been</p>
+  <p>But if you kissed me now, I know you'd fool me again</p>
+  <p>Last Christmas, I gave you my heart</p>
+  <p>But the very next day, you gave it away (you gave it away)</p>
+  <p>This year, to save me from tears</p>
+  <p>I'll give it to someone special (special)</p>
+  <p>Last Christmas, I gave you my heart</p>
+  <p>But the very next day, you gave it away</p>
+  <p>This year, to save me from tears</p>
+  <p>I'll give it to someone special (special)</p>
+  <p>Oh</p>
+  <p>Oh, oh, baby</p>
+  <p>A crowded room, friends with tired eyes</p>
+  <p>I'm hiding from you and your soul of ice</p>
+  <p>My God, I thought you were someone to rely on</p>
+  <p>Me? I guess I was a shoulder to cry on</p>
+  <p>A face on a lover with a fire in his heart</p>
+  <p>A man under cover but you tore me apart</p>
+  <p>Ooh, ooh, now I've found a real love</p>
+  <p>You'll never fool me again</p>
+  <p>Last Christmas, I gave you my heart</p>
+  <p>But the very next day, you gave it away (you gave it away)</p>
+  <p>This year, to save me from tears</p>
+  <p>I'll give it to someone special (special)</p>
+  <p>Last Christmas, I gave you my heart</p>
+  <p>But the very next day, you gave it away</p>
+  <p>This year, to save me from tears</p>
+  <p>I'll give it to someone special (special!)</p>
+  <p>A face on a lover with a fire in his heart (I gave you mine)</p>
+  <p>A man under cover, but you tore him apart</p>
+  <p>Maybe next year, we'll give it to someone</p>
+  <p>I'll give it to someone special (special!)</p>
+  <p>So long!</p>
+</div>
+`);
+
+// Little Saint Nick
+document.write(`
+<div class="lyrics-4">
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>Christmas comes this time each year</p>
+  <p>Ooh, ooh</p>
+  <p>Well way up North where the air gets cold</p>
+  <p>There's a tale about Christmas</p>
+  <p>That you've all been told</p>
+  <p>And a real famous cat all dressed up in red</p>
+  <p>And he spends the whole year workin' out on his sled</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>Just a little bobsled we call the old Saint Nick</p>
+  <p>But she'll walk a toboggan with a four speed stick</p>
+  <p>She's candy apple red with a ski for a wheel</p>
+  <p>And when Santa hits the gas, man, just watch her peel</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>Run run reindeer</p>
+  <p>Run run reindeer (whoa)</p>
+  <p>Run run reindeer</p>
+  <p>Run run reindeer (he don't miss no one)</p>
+  <p>And haulin' through the snow at a frightening speed</p>
+  <p>With a half a dozen deer with Rudy to lead</p>
+  <p>He's got to wear his goggles 'cause the snow really flies</p>
+  <p>And he's cruisin' every path with a little surprise</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>It's the little Saint Nick (little Saint Nick)</p>
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>(Christmas comes this time each year)</p>
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>(Christmas comes this time each year)</p>
+  <p>Ooh</p>
+  <p>Merry Christmas Saint Nick</p>
+  <p>(Christmas comes this time each year)</p>
+</div>
+`);
+
+// All I Want For Christmas
+document.write(`
+<div class="lyrics-5">
+    <p>I don't want a lot for Christmas</p>
+    <p>There is just one thing I need</p>
+    <p>I don't care about the presents underneath the Christmas tree</p>
+    <p>I just want you for my own</p>
+    <p>More than you could ever know</p>
+    <p>Make my wish come true</p>
+    <p>All I want for Christmas is you</p>
+    <p>Yeah</p>
+    <p>I don't want a lot for Christmas</p>
+    <p>There is just one thing I need (and I)</p>
+    <p>Don't care about the presents underneath the Christmas tree</p>
+    <p>I don't need to hang my stocking there upon the fireplace</p>
+    <p>Santa Claus won't make me happy with a toy on Christmas Day</p>
+    <p>I just want you for my own</p>
+    <p>More than you could ever know</p>
+    <p>Make my wish come true</p>
+    <p>All I want for Christmas is you</p>
+    <p>You, baby</p>
+    <p>Oh, I won't ask for much this Christmas</p>
+    <p>I won't even wish for snow (and I)</p>
+    <p>I'm just gonna keep on waiting underneath the mistletoe</p>
+    <p>I won't make a list and send it to the North Pole for Saint Nick</p>
+    <p>I won't even stay awake to hear those magic reindeer click</p>
+    <p>'Cause I just want you here tonight</p>
+    <p>Holding on to me so tight</p>
+    <p>What more can I do?</p>
+    <p>Oh, baby, all I want for Christmas is you</p>
+    <p>You, baby</p>
+    <p>Oh-oh, all the lights are shining so brightly everywhere (so brightly, baby)</p>
+    <p>And the sound of children's laughter fills the air (oh, oh, yeah)</p>
+    <p>And everyone is singing (oh, yeah)</p>
+    <p>I hear those sleigh bells ringing</p>
+    <p>Santa, won't you bring me the one I really need? (Yeah, oh)</p>
+    <p>Won't you please bring my baby to me?</p>
+    <p>Oh, I don't want a lot for Christmas</p>
+    <p>This is all I'm asking for</p>
+    <p>I just wanna see my baby standing right outside my door</p>
+    <p>Oh, I just want you for my own</p>
+    <p>More than you could ever know</p>
+    <p>Make my wish come true</p>
+    <p>Oh, baby, all I want for Christmas is you</p>
+    <p>You, baby</p>
+    <p>All I want for Christmas is you, baby</p>
+    <p>All I want for Christmas is you, baby</p>
+    <p>All I want for Christmas is you, baby</p>
+    <p>All I want for Christmas (all I really want) is you, baby</p>
+    <p>All I want (I want) for Christmas (all I really want) is you, baby</p>
+</div>
+`);
