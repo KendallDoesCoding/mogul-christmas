c2c976d83919dbd066802064ba9b22a23755744c
Kendall Pinto <kendalldoescoding@gmail.com>
2021-12-25 23:06:37 +0530

    dark mode

---
+++ dark_mode.js
@@ -0,0 +1,9 @@
+button.addEventListener('click', () => {
+document.body.classList.toggle('dark');
+localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+});
+
+if (localStorage.getItem('theme') === 'dark') {
+document.body.classList.add('dark');
+}
+
