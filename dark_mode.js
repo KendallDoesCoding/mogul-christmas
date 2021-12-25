d6e37ef83f3866c669473676e956258661b06085
Kendall Pinto <kendalldoescoding@gmail.com>
2021-12-25 23:17:30 +0530

    not needed

--- dark_mode.js
+++
@@ -1,9 +0,0 @@
-button.addEventListener('click', () => {
-document.body.classList.toggle('dark');
-localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-});
-
-if (localStorage.getItem('theme') === 'dark') {
-document.body.classList.add('dark');
-}
-
