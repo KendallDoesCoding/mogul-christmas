36d35151be6768d8a158ebfa209c39ae3a5c3d5d
aliraza944 <alisabri022@gmail.com>
2022-01-06 23:14:22 +0500

    updated the little saints nick song file

--- lyrics/wonderfultimeoftheyear/index.js
+++ lyrics/wonderfultimeoftheyear/index.js
@@ -1,6 +1,6 @@
-const songName = "the most wonderful time of the year";
-
-const song = lyrics.find((x) => x.name === songName).lyrics;
+const song = lyrics.find(
+  (x) => x.name === "the most wonderful time of the year"
+).lyrics;
 
 const lyrics__container = document.getElementsByClassName("lyrics")[0];
 
