4126f386062eb0285a724a516817b2a6371b5e8e
Kendall Pinto <kendalldoescoding@gmail.com>
2022-01-12 16:19:34 +0530

    leave space in between paragraphs for christmas song

--- lyrics/lyrics.js
+++ lyrics/lyrics.js
@@ -206,6 +206,8 @@ const lyrics = [
       "Jack Frost nipping at your nose",
       "Yuletide carols being sung by a choir",
       "And folks dressed up like Eskimos",
+      
+      " ",
       "Everybody knows a turkey and some mistletoe",
       "Help to make the season bright",
       "Tiny tots with their eyes all aglow",
@@ -214,10 +216,14 @@ const lyrics = [
       "He's loaded lots of toys and goodies on his sleigh",
       "And every mother's child is gonna spy",
       "To see if reindeers really know how to fly",
+      
+      " ",
       "And so I'm offering this simple phrase",
       "To kids from one to ninety-two",
       "Although it's been said many times, many ways",
       "Merry Christmas to you",
+      
+      " ",
       "And so I'm offering this simple phrase",
       "To kids from one to ninety-two",
       "Although it's been said many times, many ways",
