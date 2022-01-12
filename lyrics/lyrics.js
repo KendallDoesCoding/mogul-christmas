c2cb6243247a88596c3743b457580819b85a32aa
Kendall Pinto <kendalldoescoding@gmail.com>
2022-01-12 16:33:09 +0530

    remove extra lines in between

--- lyrics/lyrics.js
+++ lyrics/lyrics.js
@@ -206,7 +206,6 @@ const lyrics = [
       "Jack Frost nipping at your nose",
       "Yuletide carols being sung by a choir",
       "And folks dressed up like Eskimos",
-      
       " ",
       "Everybody knows a turkey and some mistletoe",
       "Help to make the season bright",
@@ -216,13 +215,11 @@ const lyrics = [
       "He's loaded lots of toys and goodies on his sleigh",
       "And every mother's child is gonna spy",
       "To see if reindeers really know how to fly",
-      
       " ",
       "And so I'm offering this simple phrase",
       "To kids from one to ninety-two",
       "Although it's been said many times, many ways",
       "Merry Christmas to you",
-      
       " ",
       "And so I'm offering this simple phrase",
       "To kids from one to ninety-two",
