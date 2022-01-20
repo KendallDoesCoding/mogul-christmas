331605118737a903af5a628a316d0ebc3dfa3d6a
Kendall Pinto <github@kendalldoescoding.gq>
2022-01-21 00:52:39 +0530

    Move darkmode to Javascript instead of the script tag in HTML (#133)
    Called darkmode in JS file instead of script tag, can update it further, rn its still using 3 lines for script in the HTML, also need to work on #134 soon, a enhanchment on this but mention socials instead of lyrics in the darkmode for ludsocials.

---
+++ ludsocials/darkmode.js
@@ -0,0 +1,14 @@
+const slider = document.querySelector(".slider");
+const toggle_lyrics_page = document.querySelector(".round");
+toggle_lyrics_page.addEventListener("click", modeSwitch);
+let isLight = true;
+
+function modeSwitch() {
+    isLight = !isLight;
+    isLight
+        ?
+        (slider.style.backgroundImage = "url('../images/day.png')") :
+        (slider.style.backgroundImage = "url('../images/night.png')");
+    const rootElement = document.body;
+    rootElement.classList.toggle("darkMode");
+}
