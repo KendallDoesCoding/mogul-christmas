723eb7497e975d080c7ed0ab0b7ea65292964d52
Kendall Pinto <kendalldoescoding@gmail.com>
2022-01-21 13:32:54 +0530

    added one comment

--- lyrics/darkmode.js
+++ lyrics/darkmode.js
@@ -1,3 +1,4 @@
+// Darkmode/Lightmode
 const slider = document.querySelector(".slider");
 const toggle_lyrics_page = document.querySelector(".round");
 toggle_lyrics_page.addEventListener("click", modeSwitch);
