65e9a9f739616925b373d05016da4a1cc2db8f59
aliraza944 <alisabri022@gmail.com>
2022-01-29 22:12:31 +0500

    Apply darkmode on other projects page
    fix #131
    Added the dark mode to the other project page
    - implemented the darkmode functions in the otherProjects.js
    - Added the light and dark mode variables to the CSS of the
      otherProjects
    - Added the toggle switch on the otherProjects page
    - Updated the position of the toggle switch

---
+++ scripts/otherProjects.js
@@ -0,0 +1,19 @@
+const toggle_other_projects = document.querySelector(".round");
+const slider_other_projects = document.querySelector(".slider");
+toggle_other_projects.addEventListener("click", modeSwitch);
+
+let isLight_other_projects = true;
+
+const updateModeForOtherProject = () => {
+  isLight_other_projects
+    ? (slider_other_projects.style.backgroundImage = "url('../images/day.png')")
+    : (slider_other_projects.style.backgroundImage =
+        "url('../images/night.png')");
+  const rootElement = document.body;
+  rootElement.classList.toggle("darkMode");
+};
+
+function modeSwitch() {
+  isLight_other_projects = !isLight_other_projects;
+  updateModeForOtherProject();
+}
