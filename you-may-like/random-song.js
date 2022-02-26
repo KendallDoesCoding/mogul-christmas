2456bbeb03af7d5fbae2f2b3d4c1a5c71fc23c57
KendallDoesCoding <kendalldoescoding@gmail.com>
2022-02-26 15:03:41 +0530

    Add song names in comment on top of urls in random songs file.

--- you-may-like/random-song.js
+++ you-may-like/random-song.js
@@ -3,16 +3,27 @@
 const randomSongBtn = document.getElementById("random-button");
 
 const links = [
+  // Bryan Adams - Must Be Santa
   "https://www.youtube.com/watch?v=aAuKuyAZ7YM",
+  // Bobby Helms - Jingle Bell Rock
   "https://www.youtube.com/watch?v=R_vmuL0gjU0",
+  // Pentatonix - Joy to the World
   "https://www.youtube.com/watch?v=-Xo64Q2ucQ8",
+  // Joy To The World | Original
   "https://www.youtube.com/watch?v=30OaM6b48k8",
+  // We Wish You A Merry Christmas | Original
   "https://www.youtube.com/watch?v=g-OF7KGyDis",
+  // Sam Smith - Have Yourself A Merry Little Christmas
   "https://www.youtube.com/watch?v=rnEqv8WcVq8",
+  // The Ronettes - Sleigh Ride
   "https://www.youtube.com/watch?v=JVer2lCjARU",
+  // The Ronettes - Frosty The Snowman
   "https://www.youtube.com/watch?v=PQ-ULzAAb20",
+  // Pentatonix - Frosty The Snowman ft: Alessia Cara
   "https://www.youtube.com/watch?v=18qE6_-2dQs",
+  // Michael Bublé - Frosty The Snowman (ft. The Puppini Sisters)
   "https://www.youtube.com/watch?v=jIyVB1b2aBs",
+  // Pentatonix - Mary, Did You Know?
   "https://www.youtube.com/watch?v=ifCWN5pJGIE",
 ];
 let openStuff = function () {
