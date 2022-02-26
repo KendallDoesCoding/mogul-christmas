0849ee20372513025e27623179eb3724a6c04457
KendallDoesCoding <kendalldoescoding@gmail.com>
2022-02-26 15:34:47 +0530

    Testing complete.
    It doesn't work like that xD :(

--- google-analytics.js
+++
@@ -1,9 +0,0 @@
-// Google Analytics //
-// Global site tag (gtag.js) - Google Analytics //
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-55DLRTJ04T");
