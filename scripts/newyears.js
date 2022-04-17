27f3209d1565cbab7ba921bc73e6374ed703308e
Kendall Pinto <github@kendalldoescoding.gq>
2022-04-17 16:25:23 +0530

    Update scripts/newyears.js

--- scripts/newyears.js
+++ scripts/newyears.js
@@ -1,7 +1,7 @@
 // Script to show the christmas day message on New Year's Day
 const newYearsDay = new Date();
 const messageDisplay = document.querySelector(".new_years_message");
-if (christmasDay.getMonth() === 0 && christmasDay.getDate() === 1) {
+if (newYearsDay.getMonth() === 0 && newYearsDay.getDate() === 1) {
   messageDisplay.style.display = "block";
 } else {
   messageDisplay.style.display = "none";
