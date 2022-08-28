073f8841feb2c44a5ee4487ac569c92ed8d83c96
Favour  Timothy <favourkcb@gmail.com>
2022-08-28 10:26:32 +0100

    added name attribute to the input tag for accessibity convenience

--- rating/index.js
+++ rating/index.js
@@ -1,8 +1,9 @@
 // get the input
-let rating = document.getElementById('rating')
+let const = document.getElementById('rating')
  
 // handles changes in the input and renders values to the screen 
 function handleChange(){
-    let res = document.getElementById('demo')
+    const res = document.getElementById('demo')
     res.innerHTML = rating.value
 }
+
---
+++ rating/test.js
@@ -0,0 +1,3 @@
+const title = document.querySelector('#demo')
+
+console.log(title)
