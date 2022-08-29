cb1ffbad10824b7162fe9004c1491630954cd193
Favour  Timothy <favourkcb@gmail.com>
2022-08-29 15:20:30 +0100

    redeclared some variables

--- rating/index.js
+++ rating/index.js
@@ -7,21 +7,18 @@ function handleChange() {
   res.innerHTML = rating.value;
 }
 //       Actions that takes oplace once the form is submitted 
-let submit = document.querySelector("#submit");
+const submit = document.querySelector("#submit");
 submit.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
   e.preventDefault();       // stops browser default behhaviour of automatically submitting 
 
-  if (rating.value == "0") {
-    let review_msg = document.querySelector("#review_msg");
+  if (rating.value === '0') {
+    const review_msg = document.querySelector("#review_msg");
     review_msg.style.visibility = "visible";
-    review_msg.innerText = "You must choose a rating";
-
-    return false;
-  } else {
-    // success msg on successful rating
-    review_msg.innerText = "Success";
-    review_msg.style.visibility = "visible";
+    review_msg.innerText = "You must choose a rating"
+  }else{
+    review_msg.style.visibility = 'visible'
+    review_msg.innerText = 'Success'
   }
 }
