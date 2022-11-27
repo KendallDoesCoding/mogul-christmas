4a48ede95d394bbf22419efabff7fcbbebd451e4
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-11-27 19:24:52 +0000

    christmas countdown added

--- scripts/christmasday.js
+++ scripts/christmasday.js
@@ -6,3 +6,33 @@ if (christmasDay.getMonth() === 11 && christmasDay.getDate() === 25) {
 } else {
   messageDisplay.style.display = "none";
 }
+
+
+var countDownDate = new Date("Dec 25, 2022 23:59:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  console.log(distance);
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  console.log(days);
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+ 
+  document.getElementById("christmasDayCountdown").innerHTML = days + "days to go... " ;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("christmasDayCountdown").innerHTML = "";
+  }
+}, 1000);
