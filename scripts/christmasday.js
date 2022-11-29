0de0c89b5bd4eb1b4b0ecf660d24e4eda015fef0
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-11-29 12:35:32 +0000

    Christmas countdown added (#351)
    * christmas countdown added
    
    * remove some comments
    
    * incorporated the feedback
    
    * spelling checked
    
    * dark mode background updated

--- scripts/christmasday.js
+++ scripts/christmasday.js
@@ -6,3 +6,33 @@ if (christmasDay.getMonth() === 11 && christmasDay.getDate() === 25) {
 } else {
   messageDisplay.style.display = "none";
 }
+
+/*Christmas day countdown  */
+var countDownDate = new Date("Dec 25, 2022 23:59:59").getTime();
+var now = 0;
+var distance =0;
+var days = 0;
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+   now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  distance = countDownDate - now;
+ 
+  // Time calculations for days, 
+ days = Math.floor(distance / (1000 * 60 * 60 * 24));  
+ 
+
+  // Display the result in the element with id="demo"
+ 
+  document.getElementById("christmasDayCountdown").innerHTML = days + " days left until Christmas " ;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("christmasDayCountdown").innerHTML = "";
+  }
+}, 1000);
