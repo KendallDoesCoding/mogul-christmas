0a136ede5ae7eee42f08d8377037e83ef3d54f21
Haris <mharispk321@gmail.com>
2023-04-06 20:06:21 +0500

    wrote comments for some of HTML & JS codes

--- lyrics/alliwantforchristmas/index.js
+++ lyrics/alliwantforchristmas/index.js
@@ -1,7 +1,13 @@
+// Find the lyrics for the song "all i want for christmas" in the "lyrics" array.
 const song = lyrics.find((x) => x.name === "all i want for christmas").lyrics;
 
+// Select the lyrics container from the HTML document
 const lyrics__container = document.getElementsByClassName("lyrics")[0];
+
+// Select the slider for the song "all i want for christmas"
 const slider_all_I_want_for_christmas = document.querySelector(".slider");
+
+// Create a new paragraph element for each line of the song, and append it to the lyrics container
 song.map((item) => {
   const p = document.createElement("p");
   const textNode = document.createTextNode(item);
@@ -12,8 +18,11 @@ song.map((item) => {
   return lyrics__container.appendChild(p);
 });
 
+// Select the toggle button for switching between light and dark modes
 const toggle_all_I_want_for_christmas = document.querySelector(".round");
 toggle_all_I_want_for_christmas.addEventListener("click", modeSwitch);
+
+// Set the initial mode to "light"
 let isLight_all_I_want_for_christmas = true;
 function modeSwitch() {
   isLight_all_I_want_for_christmas = !isLight_all_I_want_for_christmas;
--- lyrics/darkmode.js
+++ lyrics/darkmode.js
@@ -1,11 +1,19 @@
 // Darkmode/Lightmode
+// Select the slider element from the DOM
 const slider = document.querySelector(".slider");
+// Select the toggle element from the DOM
 const toggle_lyrics_page = document.querySelector(".round");
+// Add an event listener to the toggle element, listening for a click event
 toggle_lyrics_page.addEventListener("click", modeSwitch);
+// Initialize a boolean variable to track whether the page is currently in light mode or dark mode
 let isLight = true;
 
+// Define a function to switch between light mode and dark mode
 function modeSwitch() {
+  // Invert the value of the boolean variable
   isLight = !isLight;
+  // Select the root element from the DOM (i.e. the <body> element)
   const rootElement = document.body;
+  // Toggle the "darkMode" class on the root element
   rootElement.classList.toggle("darkMode");
 }
